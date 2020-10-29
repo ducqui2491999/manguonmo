013efb3119fff89af42a9eb23166e9e55f2e7e8d
ducqui <duyducquinguyen.24091999@gmail.com>
2020-10-29 16:53:39 +0700

    add chuvi

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -5,6 +5,12 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(float c, float d);
+int chuvihinhvuong(int a);
+int chuvihinhchunhat(int a, int b);
+int chuvihinhtron(int a);
+int dientichhinhvuong(int a);
+int dientichhinhcn(int a, int b);
+int dientichhinhtron(int a);
 
 
 
@@ -34,7 +40,40 @@ float thuong(float c, float d)
 	return c / d;
 }
 
+int chuvihinhvuong(int a)
+{
 
+	cout << "\n chu vi hinh vuong la:";
+	return a * 4;
+}
+int chuvihinhchunhat(int a, int b)
+{
+
+	cout << "\n chu vi hinh chu nhat:";
+	return  (a + b) * 2;
+}
+int chuvihinhtron(int a)
+{
+
+	cout << "\n chu vi hinh tron:";
+	return  2 * a*3.14;
+}
+int dientichhinhvuong(int a)
+{
+
+	cout << "\n dien tich hinh vuong:";
+	return a * a;
+}
+int dientichhinhcn(int a, int b)
+{
+	cout << "\ndien tich hinh chu nhat:";
+	return a * b;
+}
+int dientichhinhtron(int a)
+{
+	cout << "\n dien tich hinhn tron: ";
+	return a * a*3.14;
+}
 
 void main()
 {
@@ -48,5 +87,11 @@ void main()
 	cout << hieu(a, b);
 	cout << tich(a, b);
 	cout << thuong(a, b);
+	cout << chuvihinhvuong(a);
+	cout << chuvihinhchunhat(a, b);
+	cout << chuvihinhtron(a);
+	cout << dientichhinhvuong(a);
+	cout << dientichhinhcn(a, b);
+	cout << dientichhinhtron(a);
 	system("pause");
 }
