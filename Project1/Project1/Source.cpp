59fb3b079696193b031d6153837c3024f9f4c6bd
tuannha1999 <tuannha.1234@gmail.com>
2020-10-30 13:11:59 +0700

    add random tong

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
+#include <cstdlib> 
+#include <ctime> 
 using namespace std;
+
+void randomTong();
 
 int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(float c, float d);
-int chuvihinhvuong(int a);
-int chuvihinhchunhat(int a, int b);
-int chuvihinhtron(int a);
-int dientichhinhvuong(int a);
-int dientichhinhcn(int a, int b);
-int dientichhinhtron(int a);
 
 
+void main()
+{
+	int a, b;
+	float c, d;
+	cout << "\nNhap a, b: ";
+	cin >> a >> b;
+	cout << "\nNhap c, d : ";
+	cin >> c >> d;
+	cout << tong(a, b);
+	cout << hieu(a, b);
+	cout << tich(a, b);
+	cout << thuong(a, b);
+	randomTong();
+	system("pause");
+	return 0;
+	
+	system("pause");
+}
 
 int tong(int a, int b)
 {
@@ -40,58 +56,16 @@ float thuong(float c, float d)
 	return c / d;
 }
 
-int chuvihinhvuong(int a)
-{
 
-	cout << "\n chu vi hinh vuong la:";
-	return a * 4;
-}
-int chuvihinhchunhat(int a, int b)
+void randomTong()
 {
+	srand(time(0));
+	int r0 = rand() % 100;
+	int r1 = rand() % 100;
 
-	cout << "\n chu vi hinh chu nhat:";
-	return  (a + b) * 2;
-}
-int chuvihinhtron(int a)
-{
+	cout << "\n Số Random 1:" << r0;
+	cout << "\n Số Random 2:" << r1;
+	cout << "\n Tong Random:" << r0 + r1;
 
-	cout << "\n chu vi hinh tron:";
-	return  2 * a*3.14;
-}
-int dientichhinhvuong(int a)
-{
-
-	cout << "\n dien tich hinh vuong:";
-	return a * a;
-}
-int dientichhinhcn(int a, int b)
-{
-	cout << "\ndien tich hinh chu nhat:";
-	return a * b;
-}
-int dientichhinhtron(int a)
-{
-	cout << "\n dien tich hinhn tron: ";
-	return a * a*3.14;
 }
 
-void main()
-{
-	int a, b;
-	float c, d;
-	cout << "\nNhap a, b: ";
-	cin >> a >> b;
-	cout << "\nNhap c, d : ";
-	cin >> c >> d;
-	cout << tong(a, b);
-	cout << hieu(a, b);
-	cout << tich(a, b);
-	cout << thuong(a, b);
-	cout << chuvihinhvuong(a);
-	cout << chuvihinhchunhat(a, b);
-	cout << chuvihinhtron(a);
-	cout << dientichhinhvuong(a);
-	cout << dientichhinhcn(a, b);
-	cout << dientichhinhtron(a);
-	system("pause");
-}
